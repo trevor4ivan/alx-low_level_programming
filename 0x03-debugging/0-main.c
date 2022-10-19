0beb70c74a1981102cc7169bfe74fb6efd6be41e
trevor4ivan <olabowaleogunbanwo@gmail.com>
2022-10-19 13:32:16 -0700

    debugging task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * Main- test function
+ * Return:zero
+ *
+ */
+Int main(void)
+{ int i;
+i = 0;
+postitive_or_negative(i);
+return (0);
+}
+
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,6 @@
+#ifndef main_h
+#define main_h
+#include <stdio.h>
+Void postitive_or_negative(int i);
+#endif
+
