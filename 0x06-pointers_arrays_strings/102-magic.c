9b20b10d691eaae0ce8da23736080aad7eb6f6ac
trevor4ivan <olabowaleogunbanwo@gmail.com>
2022-10-28 13:00:12 -0700

    fixed betty

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-#include <stdio.h>
+/**
+ * main - Print integer
+ * Descripion: to print integer variables
+ * Return: 0
+ */
 
 int main(void)
 {
@@ -9,13 +13,16 @@ int *p;
 
 a[2] = 1024;
 p = &n;
-  /*
+  /**
+   * main: Print integer
+   * Descripion: to print integer variables
    * write your line of code here...
    * Remember:
    * - you are not allowed to use a
    * - you are not allowed to modify p
    * - only one statement
    * - you are not allowed to code anything else than this line of code
+   *
    */
 *(p + 5) = 98;
   /* ...so that this prints 98\n */
